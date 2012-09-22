6da39dc6321f39ad8f5142f91fe410a169373086
Nodir Turakulov <turakulov@gmail.com>
2012-09-22 18:59:54 +0500

    Added "-" operator to MefExtension

---
+++ MefExtension/Subtract.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.Composition;
+
+namespace MefCalc
+{
+    [Export(typeof(IOperator))]
+    [ExportMetadata("Symbol", '-')]
+    class Subtract : IOperator
+    {
+        public int Operate(int left, int right)
+        {
+            return left - right;
+        }
+    }
+}
